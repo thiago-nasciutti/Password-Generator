f2927ec731a2ba664b8777ceaf08f7cb72e8fcf6
thiago <nasciutti.thiago@gmail.com>
2022-07-22 09:38:09 -0400

    clean1

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,7 @@ function specs(){
   var useSymbols=confirm("Do you want to use symbols?");
  
 
+
   //Specs arrays
   var lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -16,115 +17,79 @@ function specs(){
   var symbols=["`","~","!","@","#","$","%","^","&","*","()","-","_","=","+","{","[","}",";",":","'","]","<",",",".",">","?"];
   
 
+
   //Conditionals and Arrays Merge
 
   if(useLower && useUpper && useNumbers && useSymbols){
-
     var arrays_merge = lowerCase .concat (upperCase, numbers, symbols);
-
-   // console.log(arrays_merge);
-  
   }
 
   else if(useLower && useUpper && useNumbers){
-
     var arrays_merge = lowerCase .concat (upperCase, numbers);
-
-   // console.log(arrays_merge);
-
   }
 
   else if(useLower && useUpper && useSymbols){
     var arrays_merge = lowerCase .concat (upperCase, symbols);
-
-   // console.log(arrays_merge);
   }
 
   else if(useLower && useNumbers && useSymbols){
     var arrays_merge = lowerCase .concat ( numbers, symbols);
-
-   // console.log(arrays_merge);
   }
 
   else if(useUpper && useNumbers && useSymbols){
     var arrays_merge = upperCase .concat (numbers, symbols);
-
-   // console.log(arrays_merge);
   }
 
   else if(useLower && useUpper){
     var arrays_merge = lowerCase .concat (upperCase);
-
-   // console.log(arrays_merge);
   }
 
   else if(useLower && useNumbers){
    var arrays_merge = lowerCase .concat (numbers);
-
-   // console.log(arrays_merge);
   }
 
   else if(useLower && useSymbols){
     var arrays_merge = lowerCase .concat ( symbols);
-
-    //console.log(arrays_merge);
   }
 
   else if(useUpper && useNumbers ){
     var arrays_merge = upperCase .concat (numbers);
-
-    //console.log(arrays_merge);
   }
 
   else if(useUpper && useSymbols){
     var arrays_merge = upperCase.concat (symbols);
-
-    //console.log(arrays_merge);
   } 
 
   else if(useNumbers && useSymbols){
     var arrays_merge = numbers .concat (symbols);
-
-    //console.log(arrays_merge);
   }
 
   else if(useUpper && useSymbols){
     var arrays_merge = upperCase.concat (symbols);
-
-    // console.log(arrays_merge);
   }
 
   else if(useLower){
      var arrays_merge = lowerCase;
-
-    //console.log(arrays_merge);
   }
 
   else if(useUpper){
       var arrays_merge = upperCase;
-
-     //console.log(arrays_merge);
   }
 
   else if(useNumbers){
      var arrays_merge = numbers;
-
-    //console.log(arrays_merge);
   }
 
   else if(useSymbols){
      var arrays_merge = symbols;
-
-    //console.log(arrays_merge);
   }
 
   else{
-    //console.log("Please answer the questions before procedure");
+    var arrays_merge = "Please answer the questions befor procedure"
   }
 
 
-
-
+  
   //Creating the password Array
   var passwordArray=[];
   for(i=0;i<passwordLength;i++){
