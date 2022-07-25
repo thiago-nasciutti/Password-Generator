534388cbcadb372baea7eb2595c52922f39ad11e
thiago <nasciutti.thiago@gmail.com>
2022-07-24 23:04:12 -0400

    final version

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 
 //Prompts
 function specs(){
@@ -7,8 +6,6 @@ function specs(){
   var useUpper=confirm("Do you want to use uppercase characters?");
   var useNumbers=confirm("Do you want to use numbers?");
   var useSymbols=confirm("Do you want to use symbols?");
- 
-
 
   //Specs arrays
   var lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -16,14 +13,11 @@ function specs(){
   var numbers=["0","1","2","3","4","5","6","7","8","9"];
   var symbols=["`","~","!","@","#","$","%","^","&","*","()","-","_","=","+","{","[","}",";",":","'","]","<",",",".",">","?"];
   
-
-
   //Conditionals and Arrays Merge
 
   if(useLower && useUpper && useNumbers && useSymbols){
     var arrays_merge = lowerCase .concat (upperCase, numbers, symbols);
   }
-
 
   else if(useLower && useUpper && useNumbers){
     var arrays_merge = lowerCase .concat (upperCase, numbers);
@@ -90,34 +84,23 @@ function specs(){
   }
 
   //Creating the password Array
-    var password=[];
-    for(i=0;i<passwordLength;i++){
+  var passwordArray=[];
+  for(i=0;i<passwordLength;i++){
     var randomX=Math.floor(Math.random()*arrays_merge.length);
-    password.push(arrays_merge[randomX]);
+    passwordArray.push(arrays_merge[randomX]);
     }
     
-    console.log(password);
+  console.log(passwordArray);
+
+  //convert array to spring
  
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  var text = passwordArray.toString();
+  document.getElementById("password").innerHTML = text;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
-}
 
 
 
